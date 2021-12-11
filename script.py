991439bdde04fdabfc7dbc1873382842729a16ed
Durafawd75472uhf <95949679+Durafawd75472uhf@users.noreply.github.com>
2021-12-11 09:06:51 +0530

    Update script.py

--- script.py
+++ script.py
@@ -18,7 +18,7 @@ See <i>/help</i> for commands and more details.</b>
 /start - Check if I'm alive!
 /help - Command help
 /about - Something about me!
-
+©️Cinemachanda
 
 <b>Filter Commands;</b>
 
@@ -48,13 +48,13 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @TroJanzHEX</b>
+<b>©@Cinemachanda</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : TomHolland</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b>  @Moviechanda_Ower   
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
