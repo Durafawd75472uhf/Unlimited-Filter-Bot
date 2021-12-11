137260e3323d11fcca57fff639d9d8abacd99b53
Durafawd75472uhf <95949679+Durafawd75472uhf@users.noreply.github.com>
2021-12-11 08:51:20 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -70,7 +70,7 @@ async def cb_handler(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://t.me/+G9Gdo5RmlnplOTNl")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
